94afb4c740e05638f362e9672aa7df211715f6b9
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-22 23:18:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def new_hash
-  # return an empty hash
+  new_hash = Hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  new_hash = { cookie: "carrot" }
 end
 
 def pioneer
