ca0556e7de81ebe4d5a59d42eca01c76352fba22
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-22 23:27:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  hashish = {":key" => value}# return a hash that includes the key and value parameters passed into this method
+  hashish[:name] = "value"
 end
 
 def read_from_hash(hash, key)
