739eec4015609c059e43941c3822ea38945b121c
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-23 00:33:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,10 +23,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hashish = hash[key]
-  if hash[key]
-     hash[key] += 1
-  else
-     hash[key] = 1
-  hash
+  def update_hash
+    if hash[key]
+      hash[key] += 1
+    else
+      hash[key] = 1
+  end
 end
